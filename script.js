0ec2e4416d8b21a9187c13d31bd96d8ccb475065
Tji20 <143890416+Tji20@users.noreply.github.com>
2023-11-23 23:44:00 +0700

    search script.js

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-    // Prevent the default form submission
-    event.preventDefault();
-    // Get the search input value
-    var searchInput = document.getElementById('searchInput').value;
-    // Perform your search logic here (you can replace this with your actual search logic)
-    alert('Searching for: ' + searchInput);
-    // You can add more advanced search functionality or redirect to a search results page.
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const suggestions = document.getElementById("suggestions");
+
+  const products = [
+      { name: "Đèn Trang Trí A", image: "https://denchauau.vn/media/product/den-chum-quat-thien-ha-mc-kd1364.jpg" },
+      { name: "Đèn Trang Trí B", image: "https://denchauau.vn/media/product/den-op-tran-led-hien-dai-th-p063.jpg" },
+      // Add other products as needed
+  ];
+
+  searchInput.addEventListener("input", function () {
+      const searchTerm = searchInput.value.toLowerCase();
+      const matchingProducts = products.filter(product => product.name.toLowerCase().includes(searchTerm));
+
+      suggestions.innerHTML = "";
+      matchingProducts.forEach(product => {
+          const suggestionItem = document.createElement("div");
+          suggestionItem.classList.add("suggestion-item");
+          suggestionItem.innerHTML = `<img src="${product.image}" alt="${product.name}" /> ${product.name}`;
+          suggestionItem.addEventListener("click", function () {
+              searchInput.value = product.name;
+              // You can keep the suggestions visible after a selection if needed
+          });
+          suggestions.appendChild(suggestionItem);
+      });
+
+      // suggestions.style.display = matchingProducts.length > 0 ? "block" : "none"; Remove this line
+  });
 });
-
-document.getElementById('searchInput').addEventListener('input', function () {
-    var searchInput = document.getElementById('searchInput').value;
-    if (searchInput.trim() !== '') {
-        // Call a function to fetch product suggestions based on the search input
-        fetchProductSuggestions(searchInput);
-    } else {
-        // Clear suggestions if the search input is empty
-        document.getElementById('suggestions').innerHTML = '';
-    }
-});
-
-function fetchProductSuggestions(query) {
-    // Replace the URL with the actual URL of your server endpoint
-    var url = 'https://example.com/api/products?query=' + encodeURIComponent(query);
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Display the suggestions
-            var suggestionsContainer = document.getElementById('suggestions');
-            suggestionsContainer.innerHTML = '';
-
-            data.products.forEach(function (product) {
-                var suggestionItem = document.createElement('div');
-                suggestionItem.textContent = product;
-                suggestionItem.classList.add('suggestion-item');
-
-                suggestionItem.addEventListener('click', function () {
-                    // Set the selected suggestion as the search input value
-                    document.getElementById('searchInput').value = product;
-                    // Clear the suggestions
-                    suggestionsContainer.innerHTML = '';
-                });
 
                 suggestionsContainer.appendChild(suggestionItem);
             });
